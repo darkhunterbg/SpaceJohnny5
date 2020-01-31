7344c21ff48f5f4e366788eec1ae959dab96ec90
Kaloyan Nachev <kaloyan.r.nachev@gmail.com>
2020-01-31 22:54:16 +0200

    Fixed code in wrong folder

---
+++ Assets/Code/DroneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour
+{
+    [Header("Camera Settings")]
+    public Camera Camera;
+    public float Distance;
+    public float VerticalShift;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 offset = Vector3.zero;
+        offset += -transform.forward * Distance;
+        offset += transform.up * VerticalShift;
+        Camera.transform.position = transform.position + offset;
+    }
+}
