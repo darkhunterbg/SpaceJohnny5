5877e2a63a2639feadfb0ffc0bc1dc636736dc85
Kaloyan Nachev <kaloyan.r.nachev@gmail.com>
2020-02-01 02:01:47 +0200

    New curved precision controls

--- Assets/Code/DroneController.cs
+++ Assets/Code/DroneController.cs
@@ -6,6 +6,7 @@ public class DroneController : MonoBehaviour
 {
 	public Camera Camera;
 	public GameObject MovementActivateObject;
+	public GameObject ShipVisual;
 
 	[Header("Camera Settings")]
 
@@ -17,14 +18,27 @@ public class DroneController : MonoBehaviour
 	public float Acceleration = 1f;
 	public float NoInputAcceleration = -5f;
 
+
+	public AnimationCurve MouseInputRotationRateCurve = AnimationCurve.Linear(0, 0, 1, 1);
+	public float MouseInputMaxRotationRate = 100;
+
+	public AnimationCurve NoInputRotationRateCurve = AnimationCurve.Linear(0, 0, 1, 1);
+	public float NoInputRotationRateCurveSize = 10;
+	public float NoInputRotationRate = -100;
+
 	[Header("Stats")]
 	public float Velocity;
-
+	public Vector2 RotationRate;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 
+	}
+
+	Vector2 NormalizedMousePos()
+	{
+		return (Input.mousePosition / new Vector2(Screen.width, Screen.height));
 	}
 
 	// Update is called once per frame
@@ -34,34 +48,53 @@ public class DroneController : MonoBehaviour
 
 		MovementActivateObject.SetActive(input);
 
+
+		if (input) {
+			Vector2 mousePosLinear = NormalizedMousePos() * 2.0f - Vector2.one;
+			float actualLength = MouseInputRotationRateCurve.Evaluate(mousePosLinear.magnitude) * MouseInputMaxRotationRate;
+
+			Vector2 rotAccel = mousePosLinear.normalized * actualLength;
+			RotationRate = new Vector2(-rotAccel.y, rotAccel.x) * Time.deltaTime;
+
+			//	ShipVisual.transform.localRotation = Quaternion.identity;
+			//	ShipVisual.transform.Rotate(new Vector3(-mouseOffset.y * 30, mouseOffset.x * 30, 0));
+		} else {
+			float s = RotationRate.magnitude / NoInputRotationRateCurveSize;
+			float rr = NoInputRotationRateCurve.Evaluate(s) * NoInputRotationRate;
+
+			float length = Mathf.Max(0, RotationRate.magnitude + rr * Time.deltaTime);
+			RotationRate = RotationRate.normalized * length;
+			if (RotationRate.magnitude < 1.0f)
+				RotationRate = Vector2.zero;
+		}
+
+		if (RotationRate != Vector2.zero) {
+			Vector3 rot = new Vector3(RotationRate.x, RotationRate.y, 0);
+			rot *= Time.deltaTime;
+			transform.Rotate(rot);
+		}
+
 		float accel = input ? Acceleration : NoInputAcceleration;
-
 		Velocity = Mathf.Clamp(Velocity + accel * Time.deltaTime, 0, MaxVelocity);
-
 		transform.position += Velocity * Time.deltaTime * transform.forward;
 	}
 
 	private void LateUpdate()
 	{
 		bool input = Input.GetMouseButton(0);
-
+		Vector2 mouseOffset = Vector2.zero;
 		if (input) {
 			Vector2 sceenCenter = new Vector2(Screen.width, Screen.height) / 2;
 			Vector2 mousePos = Input.mousePosition;
-			Vector2 mouseOffset = mousePos - sceenCenter;
-
+			mouseOffset = mousePos - sceenCenter;
 			mouseOffset /= 200.0f;
-
-			transform.Rotate(new Vector3(-mouseOffset.y, mouseOffset.x, 0));
 		}
 
 		Vector3 offset = Vector3.zero;
 		offset += -transform.forward * Distance;
 		offset += transform.up * VerticalShift;
 		Camera.transform.position = transform.position + offset;
-		Camera.transform.localRotation = transform.rotation;
-		//Camera.transform.forward = transform.forward;
-		//Camera.transform.up = transform.up;
-		//Camera.transform.right = transform.right;
+		Camera.transform.rotation = transform.rotation;
+		//Camera.transform.Rotate(new Vector3(-mouseOffset.y * 10, mouseOffset.x * 10, 0));
 	}
 }
