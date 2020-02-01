db6315b9764cee2cb7f8c09d0149fd69e0931e4a
krisdevs <hropenmail01@abv.bg>
2020-02-01 17:00:29 +0200

    _levelEnvironmentInstnance 2

--- Assets/Code/LevelEnvironment/LevelEnvironmentInstance.cs
+++ Assets/Code/LevelEnvironment/LevelEnvironmentInstance.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelEnvironmentInstance : MonoBehaviour
+public class LevelEnvironmentInstance
 {
 	private GameObject _instance;
 
