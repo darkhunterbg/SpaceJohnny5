c62dd64ebf0aee89b48afa9131c5e8ee5f7c6083
Nedislav Stoychev <nedislav.stoichev@gmail.com>
2020-02-01 11:23:56 +0200

    Added some simple tweens for Rotation and oscillation

---
+++ Assets/Tweens/Oscillator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+	public Vector3 OscillateAxis;
+	public float Angle;
+	public float OscillateRotateSpeed;
+
+	public bool OscillatePosition;
+	public float OscillatePositionSpeed;
+	public float PositionMult;
+	public Vector3 PositionOffset;
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (OscillatePosition) {
+			transform.localPosition = PositionOffset + new Vector3(transform.localPosition.x,
+				transform.localPosition.y,
+				Mathf.Sin(Time.time * OscillatePositionSpeed) * PositionMult * OscillateAxis.z);
+		} else {
+			transform.localRotation = Quaternion.Euler(OscillateAxis.x * Angle * Mathf.Sin(Time.time * OscillateRotateSpeed), 
+				OscillateAxis.y * Angle * Mathf.Cos(Time.time * OscillateRotateSpeed), 0);
+		}
+	}
+}
---
+++ Assets/Tweens/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+	public Vector3 RotateAxis;
+
+	public float Speed = 10f;
+
+	private float _degrees = 0;
+
+	// Update is called once per frame
+	void Update()
+	{
+		_degrees += Time.deltaTime * Speed;
+		_degrees = (_degrees % 360 + 360) % 360;
+
+		transform.localRotation = Quaternion.Euler(RotateAxis.x * _degrees, RotateAxis.y * _degrees, 0);
+	}
+}
