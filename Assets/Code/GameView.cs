5638869001baf84cd339a3ecf99c7b488f012398
Kaloyan Nachev <kaloyan.r.nachev@gmail.com>
2020-02-01 16:33:39 +0200

    Added own cursor

--- Assets/Code/GameView.cs
+++ Assets/Code/GameView.cs
@@ -9,11 +9,16 @@ public class GameView : MonoBehaviour
 
 	private GameLevel _gameLevel;
 
+	public GameObject MovementCursor;
+
+	private Canvas _canvas;
+
 	private void Start()
 	{
+		_canvas = GetComponent<Canvas>();
 		_gameLevel = FindObjectOfType<GameLevel>();
 	}
-	
+
 	public void OnTestVictoryButtonPressed()
 	{
 		Game.Instance.StartLevel("Victory");
@@ -34,5 +39,19 @@ public class GameView : MonoBehaviour
 	{
 		TimeSpan timeElapsed = TimeSpan.FromSeconds(_gameLevel.TimeElapsed);
 		TimeElapsedText.text = $"Time: {timeElapsed:mm\\:ss}";
+
+		bool mouseInput = Input.GetMouseButton(0);
+
+		Cursor.lockState = mouseInput ? CursorLockMode.Confined : CursorLockMode.None;
+		Cursor.visible = !mouseInput;
+
+
+		MovementCursor.gameObject.SetActive(!Cursor.visible);
+
+		if (MovementCursor.gameObject.activeSelf) {
+			Vector2 pos;
+			RectTransformUtility.ScreenPointToLocalPointInRectangle((RectTransform)_canvas.transform, Input.mousePosition, _canvas.worldCamera, out pos);
+			((RectTransform)MovementCursor.transform).position = _canvas.transform.TransformPoint(pos);
+		}
 	}
 }
