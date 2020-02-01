91b226713573bc33df0af6b599bcfe7e680de12a
Kaloyan Nachev <kaloyan.r.nachev@gmail.com>
2020-02-01 18:27:07 +0200

    - Screen space GUI tracker WIP

---
+++ Assets/Code/SSGUIObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SSGUIObject : MonoBehaviour
+{
+	public GameObject TrackingObject;
+
+
+	public void Update()
+	{
+		if (TrackingObject == null)
+			return;
+
+		var camera = Camera.main;
+		Vector3 distance = TrackingObject.transform.position + camera.transform.forward - camera.transform.position;
+	}
+}
