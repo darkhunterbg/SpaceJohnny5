e160d235f8cd01e09f1ac9255c585e60323e15d8
Nedislav Stoychev <nedislav.stoichev@gmail.com>
2020-02-01 16:22:45 +0200

    Added some tweens and basic asteroids to go around

---
+++ Assets/Code/RadialLayout.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[AddComponentMenu("Layout/Extensions/Radial Layout")]
+public class RadialLayout : MonoBehaviour
+{
+	public float fDistance;
+	[Range(0f, 360f)]
+	public float Angle, StartAngle;
+
+	public PingPongValue Contraction;
+
+	void Awake()
+	{
+		CalculateRadial(fDistance);
+	}
+
+	public void CalculateRadial(float distance)
+	{
+		if (transform.childCount == 0)
+			return;
+
+		int childrenToFormat = 0;
+		childrenToFormat = transform.childCount;
+
+		//float fOffsetAngle = Angle / childrenToFormat;
+
+		float fAngle = StartAngle;
+		for (int i = 0; i < transform.childCount; i++) {
+			Transform child = transform.GetChild(i);
+			Vector3 vPos = new Vector3(Mathf.Cos(fAngle * Mathf.Deg2Rad), Mathf.Sin(fAngle * Mathf.Deg2Rad), 0);
+			child.localPosition = vPos * distance;
+
+			fAngle += Angle;
+		}
+	}
+
+	private void Update()
+	{
+		CalculateRadial(Contraction.CurrentValue);
+	}
+}
---
+++ Assets/Tweens/SphereSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSpawner : MonoBehaviour
+{
+	public float MinObjects;
+	public float MaxObjects;
+	public float SphereRadius;
+	public float MinScale = 1;
+	public float MaxScale = 3;
+
+
+	public GameObject SpawnableObject;
+
+	[Header("Preview")]
+	public float SpawnObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		SpawnObjects = Random.Range(MinObjects, MaxObjects);
+
+		for (int i = 0; i < SpawnObjects; i++) {
+			GameObject spawnable = Instantiate(SpawnableObject, gameObject.transform);
+			spawnable.transform.localPosition = new Vector3(Random.Range(-SphereRadius, SphereRadius), Random.Range(-SphereRadius, SphereRadius), Random.Range(-SphereRadius, SphereRadius));
+			spawnable.transform.localScale = new Vector3(Random.Range(MinScale, MaxScale), Random.Range(MinScale, MaxScale), Random.Range(MinScale, MaxScale));
+		}
+	}
+}
