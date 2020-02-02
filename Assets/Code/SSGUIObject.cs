c2940202823e322bac9590a798569a0f040e4472
Kaloyan Nachev <kaloyan.r.nachev@gmail.com>
2020-02-02 10:37:48 +0200

    HUD added;

--- Assets/Code/SSGUIObject.cs
+++ Assets/Code/SSGUIObject.cs
@@ -35,8 +35,9 @@ public class SSGUIObject : MonoBehaviour
 		}
 
 		Vector2 size = ((RectTransform)transform).rect.size;
+		Vector2 o = new Vector2(80, 80);
 
-		bool outOfSceen = ssPos.z < 0 || ssPos.x < 0 || ssPos.x > camera.pixelWidth || ssPos.y < 0 || ssPos.y > camera.pixelHeight;
+		bool outOfSceen = ssPos.z < 0 || ssPos.x < o.x || ssPos.x > camera.pixelWidth - o.x || ssPos.y < o.y || ssPos.y > camera.pixelHeight - o.y;
 
 		if (OutOfSceenVisuals != null)
 			OutOfSceenVisuals.SetActive(outOfSceen);
@@ -44,19 +45,30 @@ public class SSGUIObject : MonoBehaviour
 		if (OnScreenVisuals != null)
 			OnScreenVisuals.SetActive(!outOfSceen);
 
-		ssPos.x = Mathf.Clamp(ssPos.x, size.x / 2, camera.pixelWidth - size.x / 2);
-		ssPos.y = Mathf.Clamp(ssPos.y, size.y / 2, camera.pixelHeight - size.y / 2);
+
+		Vector2 offset = size / 2 + o;
+
+		ssPos.x = Mathf.Clamp(ssPos.x, offset.x, camera.pixelWidth - offset.x);
+		ssPos.y = Mathf.Clamp(ssPos.y, offset.y, camera.pixelHeight - offset.y);
+
 
 		((RectTransform)transform).position = ssPos;
 
 		if (outOfSceen && OutOfSceenVisuals != null) {
+
+
 			Vector2 norm = (new Vector2(ssPos.x, ssPos.y) - new Vector2(camera.pixelWidth, camera.pixelHeight) / 2).normalized;
 
 			float rotAngle = Mathf.Atan2(norm.y, norm.x) * Mathf.Rad2Deg;// + Mathf.PI  ;
 			((RectTransform)OutOfSceenVisuals.transform).rotation = Quaternion.Euler(0, 0, rotAngle - 90);
 
 		} else if (OnScreenVisuals != null) {
-			DistanceText.text = ((TrackingObject.transform.position - DistanceFrom.position).magnitude ).ToString("0");
+			ssPos.x = Mathf.Clamp(ssPos.x, size.x / 2, camera.pixelWidth - size.x / 2);
+			ssPos.y = Mathf.Clamp(ssPos.y, size.y / 2, camera.pixelHeight - size.y / 2);
+
+			if (DistanceText != null) {
+				DistanceText.text = ((TrackingObject.transform.position - DistanceFrom.position).magnitude).ToString("0");
+			}
 		}
 	}
 }
